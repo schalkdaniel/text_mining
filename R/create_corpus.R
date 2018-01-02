388490904c5f00706f32496593626a9a426aba1d
schalkdaniel <d.schalk@mail.de>
2018-01-02 13:43:46 +0100

    add some code

--- R/create_corpus.R
+++ R/create_corpus.R
@@ -49,28 +49,29 @@ expressions.to.delete = c(
   # "." ,",", ":", "(", ")", "[", "]", "{", "}", "0", "1", "2", "3", "4", "5",
   # "6", "7", "8", "9", "'", "&", ";", "?", "!", "#", "%", "_", "|", "=", "-", 
   # "/", "$",
-  "quot",  "gt", "lt", "EN"
+  "gt", "lt", "EN"
 )
 
-# The extraction (expected time ~10h, "n = -1" means all lines):
-time.full = proc.time()
-extractTextBetweenTextTags(
-  connection  = full.wiki, 
-  out_file    = "E:/Multimedia/Datensätze/wikipedia_articles/extraction_fullwiki.txt", 
-  expressions = expressions.to.delete, 
-  n           = -1
-)
-time.full = proc.time() - time.full
+out.file.full   = "E:/Multimedia/Datensätze/wikipedia_articles/extraction_fullwiki.txt"
+out.file.simple = "E:/Multimedia/Datensätze/wikipedia_articles/extraction_simplewiki.txt"
 
-# The extraction (expected time ~1h, "n = -1" means all lines):
-time.full = proc.time()
+# time.full = proc.time()
+# extractTextBetweenTextTags(
+#   connection  = full.wiki, 
+#   out_file    = out.file.full, 
+#   expressions = expressions.to.delete, 
+#   n           = -1
+# )
+# time.full = proc.time() - time.full
+
+time.simple = proc.time()
 extractTextBetweenTextTags(
   connection  = simple.wiki, 
-  out_file    = "E:/Multimedia/Datensätze/wikipedia_articles/extraction_simplewiki.txt", 
+  out_file    = out.file.simple, 
   expressions = expressions.to.delete, 
   n           = -1
 )
-time.full = proc.time() - time.full
+time.simple = proc.time() - time.simple
 
 # Some assumptions/drawbacks:
 
@@ -84,12 +85,123 @@ time.full = proc.time() - time.full
 # Read extracted file:
 # --------------------
 
-plain.text = readr::read_lines("extraction.txt")
+plain.text = readr::read_lines(out.file.simple)
 
-# paste everything to one huge string:
-# ------------------------------------
+# Remove stopwords:
+# -----------------
 
-plain.text = paste(plain.text, collapse = " ")
+# Helper function:
+removeWords = function (str, stopwords) {
+  x = unlist(strsplit(str, " "))
+  return (paste(x[!x %in% stopwords], collapse = " "))
+}
 
-# Count words:
-sapply(gregexpr("\\W+", plain.text), length) + 1
+# Get stopwords:
+source (file = "R/extract_stopwords.R")
+
+plain.text = unlist(
+  lapply(
+    X         = plain.text, 
+    FUN       = removeWords, 
+    stopwords = stopwords
+  )
+)
+
+# Create vocabulary. Terms will be unigrams (simple words):
+# ---------------------------------------------------------
+
+# Create iterator over tokens
+tokens = text2vec::space_tokenizer(plain.text)
+
+it    = text2vec::itoken(tokens, progressbar = TRUE)
+vocab = text2vec::create_vocabulary(it)
+
+pruned.vocab = text2vec::prune_vocabulary(
+  vocabulary         = vocab, 
+  doc_proportion_max = 0.3, 
+  vocab_term_max     = 30000
+)
+
+# Train GloVe:
+# ------------
+
+# Use filtered vocabulary:
+vectorizer = text2vec::vocab_vectorizer(pruned.vocab)
+
+# use window of 20 for context words:
+tcm = text2vec::create_tcm(it, vectorizer, skip_grams_window = 10L)
+
+glove = text2vec::GlobalVectors$new(
+  word_vectors_size = 300, 
+  vocabulary        = pruned.vocab, 
+  x_max             = 100, 
+  learning_rate     = 0.03, 
+  shuffle           = 42L
+)
+
+# glove$fit doesn't work. Use fit_transform instead:
+glove$fit_transform(
+  x               = tcm, 
+  n_iter          = 20, 
+  n_threads       = parallel::detectCores()
+)
+
+# Evaluate the trained model:
+# ---------------------------
+
+# Extract word vectors:
+word_vectors = glove$components
+
+# Helper function:
+evaluateGloveOwn = function (word_vectors, a, b = NA, c = NA, norm, print.words = 5L)
+{
+  if (missing(norm)) {
+    norm = function (x) 
+    {
+      return (sqrt(sum(x^2)))
+    }
+  }
+  
+  # Check if all words are within the occured words:
+  if (! a %in% colnames(word_vectors)) {
+    stop ("Word ", a, " doesn't occur as a word vector!")
+  }
+  if ( (! b %in% colnames(word_vectors)) && (! is.na(b)) ) {
+    stop ("Word ", b, " doesn't occur as a word vector!")
+  }
+  if ( (! c %in% colnames(word_vectors)) && (! is.na(c)) ) {
+    stop ("Word ", c, " doesn't occur as a word vector!")
+  }
+  
+  if (is.na(b) || is.na(c)) {
+    # Find next words to word a:
+    compare = word_vectors[, a]
+  } else {
+    # Find word which behaves to c like a to b
+    compare = word_vectors[, b] - word_vectors[, a] + word_vectors[, c]
+  }
+  
+  dists = apply(X = word_vectors, MARGIN = 2, FUN = function (x) {
+    # Cosine similarity (https://en.wikipedia.org/wiki/Cosine_similarity)
+    crossprod(compare, x) / ( norm(compare) * norm(x) )
+    # norm(x - compare)
+  })
+  
+  if (is.numeric(print.words)) {
+    print(head(sort(dists, decreasing = TRUE), print.words))
+  }
+  return (invisible(dists))
+}
+
+# Find nearest words:
+evaluateGloveOwn(word_vectors, a = "frog")
+evaluateGloveOwn(word_vectors, a = "sister")
+
+# Find word which behaves to c like b to a:
+evaluateGloveOwn(word_vectors, a = "france", b = "paris", c = "germany")
+evaluateGloveOwn(word_vectors, "queen", "women", "king")
+
+# Test fitted model:
+# ------------------
+
+source (file = "R/model_test.R")
