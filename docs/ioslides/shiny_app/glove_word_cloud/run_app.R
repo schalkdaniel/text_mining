69f648eeb779f5814253a26fcb3bae1028465d33
schalkdaniel <d-schalk@t-online.de>
2018-03-16 10:14:44 +0100

    slightly changes

--- docs/ioslides/shiny_app/glove_word_cloud/run_app.R
+++ docs/ioslides/shiny_app/glove_word_cloud/run_app.R
@@ -6,3 +6,6 @@ options(shiny.port = 8877)
 
 # Run App:
 shiny::runApp(paste0(git.root, '/docs/ioslides/shiny_app/glove_word_cloud'))
+
+# Run in bash:
+chrome file:///C:/Users/schal/OneDrive/github_repos/text_mining/docs/ioslides/text.html#1
